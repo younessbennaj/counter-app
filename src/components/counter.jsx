edfc260257c0d2bbaf1d57eaded6fdc4b0f70970
Youness BENNAJ <younessbennaj@MacBook-Pro-de-Youness-2.local>
2018-11-05 22:16:00 +0100

    add dynamic style for the component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,13 +5,20 @@ class Counter extends Component {
     count: 0
   };
   render() {
+    let classes = this.getBadgeClasses();
     return (
       <React.Fragment>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <span className={classes}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
   }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
   formatCount() {
     const { count } = this.state;
     return this.state.count === 0 ? "Zero" : this.state.count;
