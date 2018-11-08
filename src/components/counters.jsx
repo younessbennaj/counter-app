1e9002df2d2356b651c6adefcefd3b99575643a7
Youness BENNAJ <younessbennaj@MacBook-Pro-de-Youness-2.local>
2018-11-08 11:23:42 +0100

    add reset button to rester counter

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -16,15 +16,35 @@ class Counters extends Component {
     this.setState({ counters });
   };
 
+  onIncrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  onReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
   render() {
     return (
       <div>
+        <button onClick={this.onReset} className="btn btn-primary btn-sm m-2">
+          Reset
+        </button>
         {this.state.counters.map(counter => {
           return (
             <Counter
               key={counter.id}
               counter={counter}
               onDelete={this.onDelete}
+              onIncrement={this.onIncrement}
             />
           );
         })}
